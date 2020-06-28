7d5130f092702bac5a6cfe82475dcd40d180f6a2
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 16:07:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(x)
+  sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
